320bc8baac59b7641dbe57af3db1a5f2fe8ff3cd
kilitr <Bloodwork28@gmx.de>
2019-12-08 18:04:43 +0100

    corrected Messed up Cases from Main

--- src/de/kilitr/Main.java
+++ src/de/kilitr/Main.java
@@ -1,11 +1,11 @@
 package de.kilitr;
 
 
-public class main {
-    public static void main(string[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-        graphloader graphloader = new graphloader("small_graph.graphml");
-        undirectedgraph undirectedgraph = graphloader.getundirectedgraph();
+        GraphLoader graphLoader = new GraphLoader("small_graph.graphml");
+        UndirectedGraph undirectedGraph = graphLoader.getUndirectedGraph();
 
     }
 }
