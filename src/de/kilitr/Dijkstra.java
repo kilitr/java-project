2fd6ae85d85a6c28a66c99c59d70c0a8978058a2
kilitr <Bloodwork28@gmx.de>
2020-01-09 18:00:37 +0100

    bugfixes in Dijkstra
    * removed backtracking functionality for path extraction due to bugs in basic Dijkstra algorithm
    * Comparator for Vertices in Dijkstra changed to guarantee prioirity queue to work as expected

--- src/de/kilitr/Dijkstra.java
+++ src/de/kilitr/Dijkstra.java
@@ -10,7 +10,6 @@ public class Dijkstra {
     ArrayList<Vertex> visited;
     PriorityQueue<Vertex> pQueue;
     TreeMap<String, Pair<Integer, String>> distanceAndOrigin;
-    TreeMap<String, Pair<Integer, LinkedList<Edge>>> totalDistanceAndPath;
 
     public Dijkstra(Graph g, Vertex start) {
         System.out.print("Loading Dijkstra for start=" + start.getLabel() + " ... ");
@@ -19,10 +18,8 @@ public class Dijkstra {
         visited = new ArrayList<>();
         pQueue = new PriorityQueue<>(new VertexComparator());
         distanceAndOrigin = new TreeMap<>(new TreeAlphaNumComp());
-        totalDistanceAndPath = new TreeMap<>(new TreeAlphaNumComp());
         for(Vertex v : graph.getVertices()) {
-            distanceAndOrigin.put(v.getLabel(), new Pair<>(-1, null));
-            totalDistanceAndPath.put(v.getLabel(), null);
+            distanceAndOrigin.put(v.getLabel(), new Pair<>(Integer.MAX_VALUE, null));
         }
 
         // Initialize distanceAndOrigin with start vertex for dijkstra
@@ -35,7 +32,7 @@ public class Dijkstra {
         while(!pQueue.isEmpty()) {
             inspect(pQueue.poll());
         }
-        formatResults();
+        System.out.println(distanceAndOrigin);
     }
 
     private void inspect(Vertex inspectedVertex) {
@@ -43,11 +40,10 @@ public class Dijkstra {
             for (Edge e : inspectedVertex.getEdges()) {
                 Vertex to = inspectedVertex.getTo(e);
                 Pair<Integer, String> entry = distanceAndOrigin.get(to.getLabel());
-                if(entry.getKey() > e.getWeight() || entry.getKey() == -1) {
+                int newWeight = distanceAndOrigin.get(inspectedVertex.getLabel()).getKey() + e.getWeight();
+                if(entry.getKey() > newWeight || entry.getKey() == Integer.MAX_VALUE) {
                     Pair <Integer, String> p = new Pair<>(e.getWeight(), inspectedVertex.getLabel());
-                    if(reachesStart(p, to)) {
-                        distanceAndOrigin.put(to.getLabel(), p);
-                    }
+                    distanceAndOrigin.put(to.getLabel(), p);
                 }
                 pQueue.add(to);
             }
@@ -55,48 +51,7 @@ public class Dijkstra {
         }
     }
 
-    private void formatResults() {
-        System.out.print("formatting ... ");
-        for(Vertex v : graph.getVertices()) {
-            calculateTotalCostAndPath(v);
-        }
-        System.out.println("done!");
-    }
-
-    private void calculateTotalCostAndPath(Vertex destination) {
-        if(destination == start) {
-            totalDistanceAndPath.put(start.getLabel(), new Pair<>(0, null));
-            return;
-        }
-
-        Vertex parent = graph.getVertex(distanceAndOrigin.get(destination.getLabel()).getValue());
-        if(parent == null){
-            System.out.print("not reachable");
-            return;
-        }
-
-        // Resolve path and calculate cost
-        LinkedList<Edge> path = new LinkedList<>();
-        int weight = 0;
-        try {
-            Edge partOfPath = destination.getEdgeFrom(parent);
-            path.add(0, partOfPath);
-            weight += partOfPath.getWeight();
-            for(Vertex current = parent; current != start; current = parent) {
-                parent = graph.getVertex(distanceAndOrigin.get(current.getLabel()).getValue());
-                partOfPath = current.getEdgeFrom(parent);
-                path.add(0, partOfPath); // Add in front, so we don't have to reverse ll
-                weight += partOfPath.getWeight();
-            }
-            totalDistanceAndPath.put(destination.getLabel(), new Pair<>(weight, path));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     private boolean reachesStart(Pair<Integer, String> pairToCheck, Vertex currentVertex) {
-        //System.out.println("Current Vertex = " + v.getLabel());
-        //System.out.println("Parent Vertex = " + p.getValue());
         Vertex parent = graph.getVertex(pairToCheck.getValue());
         if(pairToCheck.getValue() == this.start.getLabel()) return true;
         boolean interrupt = true;
@@ -107,31 +62,13 @@ public class Dijkstra {
         }
     }
 
-    /**
-     * @param v
-     * @return The path from the starting Vertex to the Vertex v in Form of a LinkedList of Edges
-     */
-    public LinkedList<Edge> getPathTo(Vertex v) {
-        return totalDistanceAndPath.get(v.getLabel()).getValue();
-    }
-
-    /**
-     * @param v
-     * @return The weight / distance from the starting Vertex to the Vertex v
-     */
-    public int getWeightTo(Vertex v) {
-        return totalDistanceAndPath.get(v.getLabel()).getKey();
-    }
-
 
     private class VertexComparator implements Comparator<Vertex> {
         @Override
         public int compare(Vertex v1, Vertex v2) {
-            if(v1.getEdges().isEmpty()) return -1;
-            if(v2.getEdges().isEmpty()) return 1;
-            int thisMinWeight = v1.getMinEdge().getWeight();
-            int vertexMinWeight = v2.getMinEdge().getWeight();
-            return Integer.compare(thisMinWeight, vertexMinWeight);
+            int weightV1 = distanceAndOrigin.get(v1.getLabel()).getKey();
+            int weightV2 = distanceAndOrigin.get(v2.getLabel()).getKey();
+            return Integer.compare(weightV1,weightV2);
         }
     }
 }
