259a275014947eb3425d0d6e4ecf9c75174e2762
kilitr <Bloodwork28@gmx.de>
2020-01-09 18:00:37 +0100

    bugfixes in Dijkstra
    * removed backtracking functionality for path extraction due to bugs in basic Dijkstra algorithm
    * Comparator for Vertices in Dijkstra changed to guarantee prioirity queue to work as expected

--- src/de/kilitr/Main.java
+++ src/de/kilitr/Main.java
@@ -3,19 +3,19 @@ package de.kilitr;
 
 public class Main {
     public static void main(String[] args) {
-        UndirectedGraph undirectedGraph =  new GraphLoader("implement_dijkstra.graphml").getUndirectedGraph();
-        Dijkstra uDijk = new Dijkstra(undirectedGraph, undirectedGraph.getVertex("a"));
+        UndirectedGraph undirectedGraph =  new GraphLoader("small_graph.graphml").getUndirectedGraph();
+        Dijkstra uDijk = new Dijkstra(undirectedGraph, undirectedGraph.getVertex("n0"));
         uDijk.execute();
 
-        System.out.println("Path to f = " + uDijk.getPathTo(undirectedGraph.getVertex("f")));
-        System.out.println("Weight to f = " + uDijk.getWeightTo(undirectedGraph.getVertex("f")));
+        //System.out.println("Path to f = " + uDijk.getPathTo(undirectedGraph.getVertex("f")));
+        //System.out.println("Weight to f = " + uDijk.getWeightTo(undirectedGraph.getVertex("f")));*/
 
 
-        DirectedGraph directedGraph =  new GraphLoader("implement_dijkstra.graphml").getDirectedGraph();
-        Dijkstra dDijk = new Dijkstra(directedGraph, directedGraph.getVertex("a"));
+        DirectedGraph directedGraph =  new GraphLoader("small_graph.graphml").getDirectedGraph();
+        Dijkstra dDijk = new Dijkstra(directedGraph, directedGraph.getVertex("n0"));
         dDijk.execute();
 
-        System.out.println("Path to f = " + dDijk.getPathTo(undirectedGraph.getVertex("f")));
-        System.out.println("Weight to f = " + dDijk.getWeightTo(undirectedGraph.getVertex("f")));
+        //System.out.println("Path to f = " + dDijk.getPathTo(undirectedGraph.getVertex("f")));
+        //System.out.println("Weight to f = " + dDijk.getWeightTo(undirectedGraph.getVertex("f")));
     }
 }
