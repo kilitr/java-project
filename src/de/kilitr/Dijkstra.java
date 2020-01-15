801e4dad9109d5d8b8480a17f4fb30bcb8f168c9
kilitr <bloodwork28@gmx.de>
2020-01-15 15:38:40 +0100

    refactored getPathTo
    Method now uses way simpler approach, thus is easier to understand.
    But it doesn't work in UndirectedGraph anymore - probably due to faulty implementation of class UndirectedGraph.
    
    Signed-off-by: kilitr <Bloodwork28@gmx.de>

--- src/de/kilitr/Dijkstra.java
+++ src/de/kilitr/Dijkstra.java
@@ -2,7 +2,10 @@ package de.kilitr;
 
 import javafx.util.Pair;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class Dijkstra {
     Graph graph;
@@ -41,8 +44,8 @@ public class Dijkstra {
                 else to = e.getTo();
                 Pair<Integer, String> entry = distanceAndOrigin.get(to.getLabel());
                 int newWeight = distanceAndOrigin.get(inspectedVertex.getLabel()).getKey() + e.getWeight();
-                if(entry.getKey() > newWeight || entry.getKey() == Integer.MAX_VALUE) {
-                    Pair <Integer, String> p = new Pair<>(newWeight, inspectedVertex.getLabel());
+                if (entry.getKey() > newWeight || entry.getKey() == Integer.MAX_VALUE) {
+                    Pair<Integer, String> p = new Pair<>(newWeight, inspectedVertex.getLabel());
                     distanceAndOrigin.put(to.getLabel(), p);
                     pQueue.add(to);
                 }
@@ -51,44 +54,32 @@ public class Dijkstra {
     }
 
     public LinkedList<Edge> getPathTo(Vertex v) {
-        if(distanceAndOrigin.get(v.getLabel()).getKey() != Integer.MAX_VALUE) {
-            LinkedList<Edge> rawPath = new LinkedList<>();
-            Vertex parent;
-            while (distanceAndOrigin.get(v.getLabel()).getValue() != start.getLabel()) {
-                try {
-                    parent = graph.getVertex(distanceAndOrigin.get(v.getLabel()).getValue());
-                    Edge part;
-                    if(graph instanceof UndirectedGraph){
-                        part = v.getNormalizedUndirectedEdgeTo(parent);
-                    }
-                    else if(graph instanceof DirectedGraph){
-                        part = v.getDirectedEdgeFrom(parent);
-                    }
-                    else{
-                        throw new Exception("Dijkstra was applied to object of unknown class.");
-                    }
-                    rawPath.add(0, part);
-                    v = parent;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.exit(-1);
-                }
-            }
-            try {
-                parent = graph.getVertex(distanceAndOrigin.get(v.getLabel()).getValue());
-                Edge part;
-                if(graph instanceof UndirectedGraph) part = v.getNormalizedUndirectedEdgeTo(parent);
-                else if(graph instanceof DirectedGraph) part = v.getDirectedEdgeFrom(parent);
-                else throw new Exception("Dijkstra was applied to object of unknown class.");
-                rawPath.add(0, part);
-                v = parent;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return rawPath;//return new Path(rawPath);
+        LinkedList<Edge> path = new LinkedList<>();
+        Edge edgeToParent = getEdgeTo(v);
+        path.add(0, edgeToParent);
+        assert edgeToParent != null; // TODO: prettier solution to avoid NullPointerException
+        while (edgeToParent.getFrom() != start) {
+            edgeToParent = getEdgeTo(edgeToParent.getFrom());
+            path.add(0, edgeToParent);
         }
-        return null; //TODO: or exception ?
+        return path;
+    }
+
+    /**
+     * If there exists a path to v, this will return the optimal (according to Dijkstra) Edge to v.
+     *
+     * @param v is the destination Vertex.
+     * @return Edge from the Dijkstra-Graph based parent to v.
+     */
+    private Edge getEdgeTo(Vertex v) {
+        String parentLabel = distanceAndOrigin.get(v.getLabel()).getValue();
+        Vertex parent = graph.getVertex(parentLabel);
+        for (Edge e : parent.getEdges()) {
+            if (e.getTo() == v) {
+                return e;
+            }
+        }
+        return null;
     }
 
     private class VertexComparator implements Comparator<Vertex> {
@@ -96,7 +87,7 @@ public class Dijkstra {
         public int compare(Vertex v1, Vertex v2) {
             int weightV1 = distanceAndOrigin.get(v1.getLabel()).getKey();
             int weightV2 = distanceAndOrigin.get(v2.getLabel()).getKey();
-            return Integer.compare(weightV1,weightV2);
+            return Integer.compare(weightV1, weightV2);
         }
     }
 }
